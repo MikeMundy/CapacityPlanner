49cb858d521f054305b13757543950859ecd66c9
MikeMundy <seraphrim@hotmail.com>
2022-01-16 16:29:42 -0500

    Change home page (#6)
    Co-authored-by: Michael Mundy <mmundy@west.com>

--- src/App.tsx
+++ src/App.tsx
@@ -125,7 +125,7 @@ const App: React.FC<IProps> = (props: IProps) => {
     return programIncrements;
   };
 
-  const [page, setPage] = useState("LOCATIONS");
+  const [page, setPage] = useState("HOME");
   const [personsBasic, setPersonsBasic] = useState(getInitialPersonsBasic())
   const [teams, setTeams] = useState(getInitialTeams())
   const [personTeams, setPersonTeams] = useState(getInitialPersonTeams())
